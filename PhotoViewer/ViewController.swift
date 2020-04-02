ab97133774940f427664e0aa8bf1e550d680d2eb
Kirill Shteffen <kirillshteffen@gmail.com>
2020-04-02 20:43:26 +0500

    Initial Commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by Kirill Shteffen on 02.04.2020.
+//  Copyright © 2020 Kirill Shteffen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
